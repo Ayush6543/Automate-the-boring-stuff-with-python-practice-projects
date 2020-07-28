0ee6c78023608a4c9f39079d23cbefaf2fd99761
Ayush kumar <66837202+Ayush6543@users.noreply.github.com>
2020-07-28 16:34:42 +0530

    Added Comments

--- Fuctions.py
+++ Fuctions.py
@@ -2,6 +2,7 @@ import re
 import random
 secret_number = random.randint(1, 20)
 print("I am thinking of a secret number between 1 and 20")
+# start of a loop
 for guess_taken in range(1, 7):
     guess = int(input('Guess'))
 
